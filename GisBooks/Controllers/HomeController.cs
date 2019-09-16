b7bdeaf0df58e9368ede534ea80e190a1cf6ba3a
Anton <talashmanovhm@gmail.com>
2019-09-16 22:05:06 +0500

    Initial commit

--- GisBooks/Controllers/HomeController.cs
+++ GisBooks/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Dapper;
+using GisBooks.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +13,26 @@ namespace GisBooks.Controllers
 {
     public class HomeController : Controller
     {
+        readonly string constr = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Книжечки";
+
+            var books = new List<BooksView>();
+
+            using (IDbConnection db = new SqlConnection(constr))
+            {
+                books = db.Query<BooksView>("SELECT dbo.books.title, dbo.authors.Author, dbo.genres.genre, dbo.publishers.Publisher, dbo.books.releaseYear " + 
+                                            "FROM dbo.books INNER JOIN " +
+                                                 "dbo.authors ON dbo.books.authorId = dbo.authors.id INNER JOIN " +
+                                                 "dbo.genres ON dbo.books.genreId = dbo.genres.id INNER JOIN " +
+                                                 "dbo.publishers ON dbo.books.publisherId = dbo.publishers.id").ToList();
+            }
+
+            return View(books);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ GisBooks/Models/BooksView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GisBooks.Models
+{
+    public class BooksView
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Название произведения")]
+        public string title { get; set; }
+
+        [Display(Name = "Автор")]
+        public string author { get; set; }
+
+        [Display(Name = "Жанр")]
+        public string genre { get; set; }
+
+        [Display(Name = "Издательство")]
+        public string publisher { get; set; }
+
+        [Display(Name = "Первая публикация")]
+        public string releaseYear { get; set; }
+    }
+}
